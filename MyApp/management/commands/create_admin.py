94c37f40364c1c24c3a0b89e005ca7f958c49f5b
陈云翰 <chenyunhank5@gmail.com>
2026-03-21 18:23:42 -0400

    Change admin credentials to railway_admin

--- MyApp/management/commands/create_admin.py
+++ MyApp/management/commands/create_admin.py
@@ -1,15 +1,15 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-import os
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        username = 'admin'
-        password = 'password123'
+        # Let's try a different username to be 100% sure
+        username = 'railway_admin'
+        password = 'NewPassword123!'
         email = 'admin@example.com'
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username, email, password)
-            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser "{username}"'))
+            self.stdout.write(self.style.SUCCESS(f'CREATED: {username}'))
         else:
-            self.stdout.write(self.style.WARNING(f'Superuser "{username}" already exists'))
+            self.stdout.write(self.style.WARNING(f'EXISTS: {username}'))
