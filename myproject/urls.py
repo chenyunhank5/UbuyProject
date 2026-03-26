7e42ac63d71637c688bbc018c5e65b1a2d757dbf
陈云翰 <chenyunhank5@gmail.com>
2026-03-25 22:28:34 -0400

    I Fixed My Code

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from MyApp import views
-import os  # <--- MUST HAVE THIS
+
 urlpatterns = [
     # --- ADMIN ---
     path('admin/', admin.site.urls),
@@ -64,6 +64,6 @@ urlpatterns = [
 ]
 
 # --- STATIC / MEDIA ---
-if settings.DEBUG or os.getenv('RAILWAY_ENVIRONMENT'):
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
